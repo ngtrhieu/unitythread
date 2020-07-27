4e86fe9ad270572b45c6a394594d10dd456051cc
ngtrhieu <ngtrhieu0011@gmail.com>
2020-07-27 13:15:20 +0800

    feat: add UnityThread package

---
+++ UnityThread/Runtime/UnityThread.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnityEngine {
+
+  ///<summary>
+  ///Helper class to execute lambdas in Unity's main thread.
+  ///</summary>
+  public class UnityThread : MonoBehaviour {
+
+    private static UnityThread current;
+
+    private static List<System.Action> updateLoopQueue = new List<Action>();
+    private static List<System.Action> lateUpdateLoopQueue = new List<Action>();
+    private static List<System.Action> fixedUpdateLoopQueue = new List<Action>();
+
+    private static List<System.Action> tempQueue = new List<System.Action>();
+
+    private static UnityThread CreateUnityThreadInstance(bool visible = true) {
+      if (current != null) return current;
+
+      if (Application.isPlaying) {
+        var obj = new GameObject("UnityThreadRunner");
+        obj.hideFlags = visible ? HideFlags.HideAndDontSave : HideFlags.None;
+
+        current = obj.AddComponent<UnityThread>();
+      }
+      return current;
+    }
+
+    private void Awake() {
+      if (current == null || current == this)
+        current = this;
+      else {
+        Destroy(gameObject);
+        return;
+      }
+
+      DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update() {
+      if (updateLoopQueue.Count == 0)
+        return;
+
+      tempQueue.Clear();
+      lock (updateLoopQueue) {
+        tempQueue.AddRange(updateLoopQueue);
+        updateLoopQueue.Clear();
+      }
+
+      foreach (var action in tempQueue)
+        action.Invoke();
+    }
+
+    private void LateUpdate() {
+      if (lateUpdateLoopQueue.Count == 0)
+        return;
+
+      tempQueue.Clear();
+      lock (lateUpdateLoopQueue) {
+        tempQueue.AddRange(lateUpdateLoopQueue);
+        lateUpdateLoopQueue.Clear();
+      }
+
+      foreach (var action in tempQueue)
+        action.Invoke();
+    }
+
+    private void FixedUpdate() {
+      if (fixedUpdateLoopQueue.Count == 0)
+        return;
+
+      tempQueue.Clear();
+      lock (fixedUpdateLoopQueue) {
+        tempQueue.AddRange(fixedUpdateLoopQueue);
+        fixedUpdateLoopQueue.Clear();
+      }
+
+      foreach (var action in tempQueue)
+        action.Invoke();
+    }
+
+    public static void ExecuteCoroutine(IEnumerator action) {
+      if (action == null)
+        return;
+
+      CreateUnityThreadInstance();
+
+      ExecuteInUpdate(() => current.StartCoroutine(action));
+    }
+
+    public static void ExecuteInUpdate(System.Action action) {
+      if (action == null)
+        return;
+
+      CreateUnityThreadInstance();
+
+      lock (updateLoopQueue) {
+        updateLoopQueue.Add(action);
+      }
+    }
+
+    public static void ExecuteInLateUpdate(System.Action action) {
+      if (action == null)
+        return;
+
+      CreateUnityThreadInstance();
+
+      lock (lateUpdateLoopQueue) {
+        lateUpdateLoopQueue.Add(action);
+      }
+    }
+
+    public static void ExecuteInFixedUpdate(System.Action action) {
+      if (action == null)
+        return;
+
+      CreateUnityThreadInstance();
+
+      lock (fixedUpdateLoopQueue) {
+        fixedUpdateLoopQueue.Add(action);
+      }
+    }
+
+  }
+}
---
+++ UnityThread/Tests/Runtime/UpdateLoopTest.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+  public class UpdateLoopTest {
+
+    [UnityTest]
+    public IEnumerator ActionExecuteNextUpdate() {
+      var counter = 0;
+      UnityThread.ExecuteInUpdate(() => {
+        ++counter;
+        Debug.Log("Update loop");
+      });
+
+      // Runner is spawned
+      var runner = GameObject.Find("UnityThreadRunner");
+      Assert.NotNull(runner);
+
+      // Counter not yet modified
+      Assert.AreEqual(0, counter);
+
+      yield return null;
+
+      // Logged
+      LogAssert.Expect(LogType.Log, "Update loop");
+
+      // Counter increased
+      Assert.AreEqual(1, counter);
+
+      yield return null;
+
+      // Counter not increased any further
+      Assert.AreEqual(1, counter);
+    }
+
+    [UnityTest]
+    public IEnumerator ActionExecuteNextLateUpdate() {
+
+      // Wait for a new frame
+      yield return new WaitForEndOfFrame();
+
+      var counter = 0;
+      UnityThread.ExecuteInUpdate(() => {
+        ++counter;
+        Debug.Log("Update loop");
+      });
+      UnityThread.ExecuteInLateUpdate(() => {
+        counter = counter * 2;
+        Debug.Log("LateUpdate loop");
+      });
+
+      // Runner is spawned
+      var runner = GameObject.Find("UnityThreadRunner");
+      Assert.NotNull(runner);
+
+      yield return null;
+      LogAssert.Expect(LogType.Log, "Update loop");
+      Assert.AreEqual(1, counter);
+
+      yield return new WaitForEndOfFrame();
+      LogAssert.Expect(LogType.Log, "LateUpdate loop");
+      Assert.AreEqual(2, counter);
+    }
+
+    [UnityTest]
+    public IEnumerator ActionExecuteNextFixedUpdate() {
+
+      // Wait for a new fixed frame
+      yield return new WaitForFixedUpdate();
+
+      Time.fixedDeltaTime = 0.33f;
+
+      var counter = 0;
+      UnityThread.ExecuteInFixedUpdate(() => {
+        counter += 10;
+        Debug.Log("FixedUpdate loop");
+      });
+
+      // Runner is spawned
+      var runner = GameObject.Find("UnityThreadRunner");
+      Assert.NotNull(runner);
+
+      yield return null;
+      Assert.AreEqual(0, counter);
+
+      yield return new WaitForEndOfFrame();
+      Assert.AreEqual(0, counter);
+
+      yield return new WaitForFixedUpdate();
+      LogAssert.Expect(LogType.Log, "FixedUpdate loop");
+      Assert.AreEqual(10, counter);
+    }
+  }
+}
