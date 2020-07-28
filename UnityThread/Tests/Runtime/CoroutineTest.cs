988be412bbd4061720f10d8370542c5459e66d86
ngtrhieu <ngtrhieu0011@gmail.com>
2020-07-28 13:25:27 +0800

    test: add coroutine test

---
+++ UnityThread/Tests/Runtime/CoroutineTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace UnityEngine.Tests {
+  public class CoroutineTest {
+
+    private int counter = 0;
+
+    private IEnumerator ACoroutine() {
+      while (counter < 5) {
+        yield return new WaitForSeconds(0.3f);
+        counter++;
+      }
+    }
+
+    [UnityTest]
+    public IEnumerator CoroutineExecutedNextUpdate() {
+      UnityThread.ExecuteCoroutine(ACoroutine());
+
+      // Runner is spawned
+      var runner = GameObject.Find("UnityThreadRunner");
+      Assert.NotNull(runner);
+
+      // Counter not yet modified
+      Assert.AreEqual(0, counter);
+
+      yield return null;
+
+      // 0.3 sec has not passed, counter should not been increased
+      Assert.AreEqual(0, counter);
+
+      yield return new WaitForSeconds(0.3f);
+      Assert.AreEqual(1, counter);
+      yield return new WaitForSeconds(0.4f);
+      Assert.AreEqual(2, counter);
+      yield return new WaitForSeconds(0.6f);
+      Assert.AreEqual(4, counter);
+      yield return new WaitForSeconds(1f);
+      Assert.AreEqual(5, counter);
+    }
+  }
+}
--- UnityThread/Tests/Runtime/UpdateLoopTest.cs
+++ UnityThread/Tests/Runtime/UpdateLoopTest.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Tests {
+namespace UnityEngine.Tests {
   public class UpdateLoopTest {
 
     [UnityTest]
